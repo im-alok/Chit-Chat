fcbe66b0013df2a224c91d905c955adfe52eed18
im-alok <alok.ranjan.aiml25@heritageit.edu.in>
2025-02-12 23:11:55 +0530

    node mailer init

---
+++ Backend/src/config/nodemailer.ts
@@ -0,0 +1,26 @@
+import nodemailer from 'nodemailer'
+import dotenv from 'dotenv'
+dotenv.config();
+
+
+export const sendMail = async(email:string,title:string,body:string)=>{
+    try {
+        let transporter = nodemailer.createTransport({
+            host:process.env.HOST,
+            auth:{
+                user:process.env.USER,
+                pass:process.env.PASSWORD
+            }
+        })
+
+        let info = await transporter.sendMail({
+            from: 'Connect & Chat || Connect with world',
+            to:`${email}`,
+            subject:`${title}`,
+            html:`${body}`
+        })
+
+    } catch (error:any) {
+        console.log(error.message)
+    }
+}
